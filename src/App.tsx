7cea236ab21ed23e3c4db5ae8fb0884311e25d3b
Ryan Blyth <ryan.blyth@myenergi.com>
2023-09-04 09:30:14 +0100

    chore: initialise project with dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { ProgressBar } from './components/ProgressBar/ProgressBar';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ProgressBar
+        departureTime={'09-03-2023'}
+        arrivalTime={'09-05-2023'}
+        portOfDischarging='endingPlace'
+        portOfLoading='startingPlace'
+      />
     </div>
   );
-}
+};
 
 export default App;
